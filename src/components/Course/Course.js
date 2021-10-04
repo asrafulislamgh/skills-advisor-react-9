45d743ae9c55d40fa655109d2d0121e383d1edfd
asrafulislamgh <gdmamun@yahoo.com>
2021-10-04 21:11:59 +0600

    instructor img added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -4,14 +4,29 @@ import Rating from "react-rating";
 import "./Course.css";
 
 const Course = (props) => {
-  const { name, img, students, totalCourses, rating, instructor } =
-    props.course;
+  const {
+    name,
+    img,
+    students,
+    totalCourses,
+    rating,
+    instructor,
+    cost,
+    instructorImg,
+  } = props.course;
   return (
     <div className="mb-5 rounded">
-      <Card>
-        <Card.Img variant="top" src={img} />
+      <Card className="position-relative">
+        <div className="inst-img-container">
+          <Card.Img variant="top" src={img} />
+          <Card.Img
+            className="instructors-img"
+            variant="top"
+            src={instructorImg}
+          />
+        </div>
         <Card.Body>
-          <Card.Title>{name}</Card.Title>
+          <Card.Title className="mt-3">{name}</Card.Title>
           <Card.Text>
             <p>
               Total Courses: <b>{totalCourses}</b>
@@ -21,6 +36,9 @@ const Course = (props) => {
             </p>
             <p>
               Instructor: <b>{instructor}</b>
+            </p>
+            <p>
+              Cost: <b>${cost}</b>
             </p>
             <p>
               Rating:
